62404c201fd4f57d76215413ecd66510be0dcc4a
Krystian Kościelecki <getek1382001@gmail.com>
2023-03-19 18:21:18 +0100

    crate dashboard and started create onboard

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-const AuthModal = ({setShowModal}) => {
+const AuthModal = ({setShowModal, isSignUp}) => {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [error, setError] = useState(null)
+
+    console.log(email, password, confirmPassword)
 
     const handleClick = () =>{
         setShowModal(false)
@@ -8,10 +14,17 @@ const AuthModal = ({setShowModal}) => {
 
     const handleSubmit = (e) =>{
         e.preventDefault()
+        try {
+            if(isSignUp && (password !== confirmPassword)){
+                setError("Password need to match!")
+            }
+            console.log('make a post request to our databae')
+
+        }catch (error){
+            console.log(error)
+        }
     }
 
-
-    const isSignUp = true
 
     return(
         <div className="auth-modal">
@@ -25,9 +38,30 @@ const AuthModal = ({setShowModal}) => {
                     name="email"
                     placeholder="email"
                     required={true}
-                    omChange{(e) => setEmail(e.target.value)}
+                    onChange = {(e) => setEmail(e.target.value)}
                 />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange = {(e) => setPassword(e.target.value)}
+                />
+                {isSignUp &&<input
+                    type="password"
+                    id="password-check"
+                    name="password-check"
+                    placeholder="confirm password"
+                    required={true}
+                    onChange = {(e) => setConfirmPassword(e.target.value)}
+                />}
+                <input className="secondary-button" typ="submit"/>
+                <p>{error}</p>
+                <hr/>
             </form>
+                <h2>GET THE APP</h2>
+
 
             AUTH MOAL
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,22 @@
 import whiteLogo from '../images/tinder_logo_white.png'
 import colorLogo from '../images/color-logo-tinder.png'
 
-const Nav= ({minimal, authToken, setShowModal, showModal}) => {
+const Nav= ({minimal, setShowModal, showModal, setIsSignUp}) => {
 
     const handleClick = () => {
         setShowModal(true)
+        setIsSignUp(false)
     }
 
+    const authToken = false
     return(
         <nav>
             <div className="logo-container">
-                <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
+                <img className="logo"
+                     src={minimal ? colorLogo : whiteLogo}/>
             </div>
 
-            {!authToken && minimal && <button
+            {!authToken && !minimal && <button
                 className="nav-button"
                 onClick={handleClick}
                 disabled={showModal}

5565a829692caa78aa00d66bdc23e4a32dae35b3
Krystian Kościelecki <getek1382001@gmail.com>
2023-03-19 11:10:13 +0100

    pages and routing home, autho modal

---
+++ src/components/AuthModal.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const AuthModal = ({setShowModal}) => {
+
+    const handleClick = () =>{
+        setShowModal(false)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+
+    const isSignUp = true
+
+    return(
+        <div className="auth-modal">
+            <div className="close-icon" onClick={handleClick}>ⓧ</div>
+            <h2>{isSignUp ? 'CREATE ACCOUNT' : 'LOG IN'}</h2>
+            <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    required={true}
+                    omChange{(e) => setEmail(e.target.value)}
+                />
+            </form>
+
+            AUTH MOAL
+        </div>
+    )
+}
+
+export default AuthModal

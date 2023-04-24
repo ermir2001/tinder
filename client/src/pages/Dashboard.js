5b000f1dbf9eb56038af54a098f0052a1741b18a
ermir2001 <getek1382001@gmail.com>
2023-04-24 19:27:22 +0200

    try vercel

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,112 @@
+import TinderCard from "react-tinder-card";
+import { useEffect, useState } from "react";
+import {useCookies} from "react-cookie";
+import ChatContainer from "../components/ChatContainer"
+import axios from "axios";
+
+const Dashboard = () => {
+    const [user,setUser] = useState(null)
+    const [genderedUsers, setGenderedUsers] = useState(null)
+    const [lastDirection, setLastDirection] = useState()
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
+
+    const userId = cookies.UserId
+    const getUser = async () =>{
+
+        try{
+            const response = await axios.get('http://localhost:8000/user', {
+                params: { userId }
+            })
+
+            setUser(response.data)
+        }catch (err){
+            console.log(err)
+        }
+    }
+
+    const getGenderedUsers = async () => {
+        try{
+            const response = await axios.get('http://localhost:8000/gendered-users', {
+                params: { gender: user?.gender_interest}
+            })
+            setGenderedUsers(response.data)
+        } catch (err){
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            getGenderedUsers()
+        }
+    }, [user])
+
+
+    const updateMatches = async (matchedUserId) => {
+        try{
+            await axios.put('https://localhost:8000/addmatch', {
+                userId,
+                matchedUserId
+            })
+            getUser()
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    console.log(user)
+
+
+    const swiped = (direction, swipedUserId) => {
+        if(direction === 'right'){
+            updateMatches(swipedUserId)
+        }
+
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+    const matchedUserIds = user?.matches.map(({user_id}) => user_id).concat(userId)
+
+    const filteredGenderUsers = genderedUsers?.filter(
+        genderedUser => !matchedUserIds.includes(genderedUser.user_id)
+    )
+
+    return(
+        <>
+            {user &&
+                <div className="dashboard">
+                    <ChatContainer user={user}/>
+                    <div className="swipe-container">
+                        <div className='cardContainer'>
+
+                            {filteredGenderUsers?.map((genderedUser) =>
+                                <TinderCard
+                                    className='swipe'
+                                    key={genderedUser.user_id}
+                                    onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
+                                    onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}>
+                                    <div style={{backgroundImage: 'url(' + genderedUser.url + ')'}} className='card'>
+                                        <h3>{genderedUser.first_name}</h3>
+                                    </div>
+                                </TinderCard>
+                            )}
+                            <div className="swipe-info">
+                                {lastDirection ? <p>You swiped {lastDirection}</p> : <p/>}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+
+export default Dashboard
